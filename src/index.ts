00fdbb107b628b1b8afe382848860e4ee1a8ad26
Julia Pitts <julia@robo.church>
2019-02-02 21:25:57 -0800

    Added Mithril support.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hi!");
+import * as m from "mithril";
+
+function createView() {
+  return {
+    view() {
+      return m("div", "This is my content");
+    }
+  };
+}
+
+m.mount(document.body, createView());
