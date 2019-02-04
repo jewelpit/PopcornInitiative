c81a3fe3bb3138cd79f0d7f4f36ba44197e7f7bf
Julia Pitts <julia.pitts@outlook.com>
2019-02-03 18:49:59 -0800

    Added removing combatants.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,59 @@
 import * as m from "mithril";
 
-const entities = [
-  "Dissection Dissertator",
-  "Illuminated Bun-Sen",
-  "Illuminated Re-Sear-Char"
-];
+interface SingleState {}
 
-function createView() {
-  return {
-    view() {
-      return m(
-        "ul",
-        entities.map(entity =>
-          m(
-            "li",
-            {
-              onclick: function() {
-                entities.push("Click!");
-              }
-            },
-            entity
-          )
-        )
-      );
-    }
-  };
+class GameState {
+  private _undoStack: SingleState[];
+  private _index: number;
+
+  constructor(init: SingleState) {
+    this._undoStack = [init];
+    this._index = 0;
+  }
 }
 
-m.mount(document.body, createView());
+class App {
+  private _activeGame: GameState | null;
+
+  constructor(private _entities: string[]) {
+    this._activeGame = null;
+  }
+
+  view() {
+    return m(
+      ".wrapper",
+      this._activeGame == null ? this._main() : App._game(this._activeGame)
+    );
+  }
+
+  private _main() {
+    return m(
+      "div",
+      m("h1", "Combatants"),
+      this._entities.map((entity, idx) =>
+        m(
+          ".entity",
+          entity,
+          m(
+            ".close-button",
+            { onclick: () => this._entities.splice(idx, 1) },
+            "X"
+          )
+        )
+      )
+    );
+  }
+
+  private static _game(activeGame: GameState) {
+    return m("Active game");
+  }
+}
+
+m.mount(
+  document.body,
+  new App([
+    "Dissection Dissertator",
+    "Illuminated Bun-Sen",
+    "Illuminated Re-Ser-Char"
+  ])
+);
